d4c08e398266c30c86ea630ceee697014c0a93be
Edward Arias <jayson@admins-MacBook-Pro.local>
2022-07-22 12:33:03 -0400

    first commit

---
+++ JustCode/two_digit_number/two_digit_number.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+# converted_number = int(two_digit_number)
+# print(type(converted_number))
+
+first_num = int(two_digit_number[0])
+second_num = int(two_digit_number[1])
+sum_num = first_num + second_num
+
+print(sum_num)
